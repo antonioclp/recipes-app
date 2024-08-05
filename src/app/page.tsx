8ecae3c189ba9c0b5c87c4f8d54701f10289f33a
Antonio Santana <antonionetosg6@gmail.com>
2024-08-05 16:25:37 -0300

    chore: version @0.1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+/**
+ * Development branch @version 0.1.0
+ * Home page component that renders the recipes and other features.
+ * @returns {JSX.Element}
+ */
+export default function Home(): JSX.Element {
   return (
     <main>
       <span>Home</span>
