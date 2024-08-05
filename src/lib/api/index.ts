2fe9b603a1c1be6db92bf2beb0a56dbbeef590a8
Antonio Santana <antonionetosg6@gmail.com>
2024-08-05 18:21:28 -0300

    feat: meal areas and meal random return api

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,1 +1,90 @@
-export const fetchApi = () => {}
+// Utils.
+import {getApiUrl} from '../utils'
+
+// Interfaces.
+import {IDefaultResponse, IFetchObject} from '../interfaces'
+
+export const fetchApi = async (
+  object: IFetchObject,
+): Promise<IDefaultResponse> => {
+  let status = 0
+  const {option, queryParams} = object
+
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  try {
+    if (option === 'meal-categories') {
+      const url = getApiUrl(option)
+
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        status = response.status
+        throw new Error('ocurred a internal error.')
+      }
+
+      status = response.status
+      const data = await response.json()
+
+      return {
+        message: 'sucessfully fetched.',
+        status,
+        res: data,
+      }
+    }
+
+    if (option === 'meal-areas') {
+      const url = getApiUrl(option)
+
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        status = response.status
+        throw new Error('ocurred a internal error.')
+      }
+
+      status = response.status
+      const data = await response.json()
+
+      return {
+        message: 'sucessfully fetched.',
+        status,
+        res: data,
+      }
+    }
+
+    if (option === 'meal-random') {
+      const url = getApiUrl(option)
+
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        status = response.status
+        throw new Error('ocurred a internal error.')
+      }
+
+      status = response.status
+      const data = await response.json()
+
+      return {
+        message: 'sucessfully fetched.',
+        status,
+        res: data,
+      }
+    }
+
+    status = 505
+
+    return {
+      message: 'provide a valid option.',
+      status,
+      res: null,
+    }
+  } catch (err) {
+    return {
+      message: err as string,
+      status,
+      res: null,
+    }
+  }
+}
